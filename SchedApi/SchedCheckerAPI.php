4587f5f0b1349a16bd4dd0cb6b56c87a34019fd0
gamemedecine <152599710+gamemedecine@users.noreply.github.com>
2024-09-24 20:49:35 +0800

    Add files via upload

--- SchedApi/SchedCheckerAPI.php
+++ SchedApi/SchedCheckerAPI.php
@@ -1,9 +1,8 @@
 <?php
 include("../databse.php");
 
-$JSONDATA = file_get_contents(filename: "php://input");
-
-$DCodeJSON = json_decode(json: $JSONDATA, associative: true);
+$JSONDATA = file_get_contents("php://input");
+$DCodeJSON = json_decode($JSONDATA, true);
 
 if (isset($DCodeJSON["day"]) && isset($DCodeJSON["start_time"]) && isset($DCodeJSON["therapists_id"])) {
     $days = $DCodeJSON["day"];
@@ -11,26 +10,40 @@ if (isset($DCodeJSON["day"]) && isset($DCodeJSON["start_time"]) && isset($DCodeJ
     $therapists_id = intval($DCodeJSON["therapists_id"]);
     $formattedStartTime = date_format(date_create($start_time), "H:i:s");
 
-    
-    $var_SchedChk = "SELECT * FROM `tbl_sched`
-     WHERE `therapists_id` = $therapists_id AND `start_time` = '$formattedStartTime';";
+    $var_SchedChk = "SELECT * FROM `tbl_sched` WHERE `therapists_id` = $therapists_id;"; 
 
-    $results = mysqli_query($var_conn,  $var_SchedChk);
+    $results = mysqli_query($var_conn, $var_SchedChk);
     $isMatched = 0;
 
     if ($results) {
         foreach ($results as $result) {
-            $databaseDays = explode(",",$result["day"]);
+            $databaseDays = explode(",", $result["day"]);
             $databaseStartTime = $result["start_time"];
-            $databaseTherapistId = $result["therapists_id"];
-            $var_chekDays = array_intersect($databaseDays,$days);
-            if ($var_chekDays !="" && $databaseStartTime == $formattedStartTime) {
-                $isMatched = 1;
-                break;
+            $databaseEndTime = $result["end_time"];
+
+            // Convert times to 24-hour format for easier comparison
+            $var_Stime = date("H:i:s", strtotime($databaseStartTime)); // Start time
+            $var_Etime = date("H:i:s", strtotime($databaseEndTime));   // End time
+            $var_Inputtedtime = date("H:i:s", strtotime($formattedStartTime)); // Inputted time
+
+            // Debug output
+
+            $var_chekDays = array_intersect($databaseDays, $days);
+
+            if (!empty($var_chekDays)) {
+                if ($var_Inputtedtime > $var_Stime && $var_Inputtedtime <= $var_Etime) {
+                    $isMatched = 2; 
+                    break;
+                }
+
+                if ($var_Inputtedtime == $var_Stime) {
+                    $isMatched = 1; 
+                    break;
+                }
             }
         }
 
-        echo $isMatched;
+        echo $isMatched; 
     } else {
         echo "Something went wrong while fetching the database.";
     }
